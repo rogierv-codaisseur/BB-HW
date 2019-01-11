855818d0cf43749b911a48c7e641f65d99fde2c0
Rogier Verkaik <rogierv@gmail.com>
2019-01-11 21:05:39 +0100

    Implementing the basic game logic and UI

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -1,1 +1,27 @@
 // Write your JS here
+
+const hero = {
+  name: "",
+  heroic: true,
+  inventory: [],
+  health: 10,
+  weapon: {
+    type: "",
+    damage: 2
+  }
+};
+
+function rest(objHero) {
+  objHero.health = 10;
+  return objHero;
+};
+
+function pickUpItem(objHero, objWeapon) {
+  objHero.inventory.push(objWeapon);
+};
+
+function equipWeapon(objHero) {
+  if (objHero.inventory.length !== 0) {
+    objHero.weapon = objHero.inventory[0]
+  };
+};
