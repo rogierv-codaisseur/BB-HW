81bbb3febdc735497bcbb841117e0d5f7718752b
Rogier Verkaik <rogierv@gmail.com>
2019-01-11 23:23:46 +0100

    Implement displayStats and add form to change Hero name

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -13,37 +13,52 @@ const hero = {
 
 function rest(objHero) {
   objHero.health = 10;
-  // displayStats(objHero)
+  console.log(objHero);
   return objHero;
 };
 
 function pickUpItem(objHero, objWeapon) {
   objHero.inventory.push(objWeapon);
-  // displayStats(objHero);
 };
 
 function equipWeapon(objHero) {
   if (objHero.inventory.length !== 0) {
-    objHero.weapon = objHero.inventory[0]
-    // displayStats(objHero);
+    objHero.weapon = objHero.inventory[0];
   };
 };
 
-// Write displayStats function that writes your hero's name, health, weapontype, weapon damage to the page. Call it at the end of your script
-// function displayStats(objHero) {
-//   const heroStats = document.getElementById('heroStats');
-//   const heroName = document.createElement('p');
-//   const heroHealth = document.createElement('p');
-//   const weaponType = document.createElement('p');
-//   const weaponDamage = document.createElement('p');
-//   heroStats.appendChild(heroName);
-//   heroStats.appendChild(heroHealth);
-//   heroStats.appendChild(weaponType);
-//   heroStats.appendChild(weaponDamage);
-//   heroName.innerText = "Name: " + objHero.name;
-//   heroHealth.innerText = "Health: " + objHero.health;
-//   weaponType.innerText = "Weapon Type: " + objHero.weapon.type;
-//   weaponDamage.innerText = "Weapon Damage: " + objHero.weapon.damage;
-// };
+function changeName(objHero) {
+  event.preventDefault();
+  inputField = document.getElementById('change-name');
+  if (!inputField.value) {
+    alert("Enter a name");
+    return false;
+  }
+  objHero.name = inputField.value;
+  inputField.value = null;
+}
 
-// displayStats(hero);
+// Write displayStats function that writes your hero's name, health, weapontype, weapon damage to the page. Call it at the end of your script
+function displayStats(objHero) {
+  const heroStats = document.getElementById('heroStats');
+
+  // Remove all child elements of a DOM node
+  while (heroStats.firstChild) {
+    heroStats.removeChild(heroStats.firstChild);
+  }
+
+  const heroName = document.createElement('p');
+  const heroHealth = document.createElement('p');
+  const weaponType = document.createElement('p');
+  const weaponDamage = document.createElement('p');
+  heroStats.appendChild(heroName);
+  heroStats.appendChild(heroHealth);
+  heroStats.appendChild(weaponType);
+  heroStats.appendChild(weaponDamage);
+  heroName.innerText = "Name: " + objHero.name;
+  heroHealth.innerText = "Health: " + objHero.health;
+  weaponType.innerText = "Weapon Type: " + objHero.weapon.type;
+  weaponDamage.innerText = "Weapon Damage: " + objHero.weapon.damage;
+};
+
+displayStats(hero);
