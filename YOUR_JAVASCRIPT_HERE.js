547feb6d8035c358e8c6b76b4f86a4e892d121a6
Rogier Verkaik <rogierv@gmail.com>
2019-01-12 10:13:23 +0100

    Add enemies and fighting features

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -8,12 +8,45 @@ const hero = {
   weapon: {
     type: "",
     damage: 2
-  }
+  },
+  accuracy: 0.4
+};
+
+const enemy1 = {
+  id: "enemy1",
+  name: "Brutus",
+  health: 5,
+  weapon: {
+    type: "",
+    damage: 4
+  },
+  accuracy: 0.35
+};
+
+const enemy2 = {
+  id: "enemy2",
+  name: "Pegleg",
+  health: 3,
+  weapon: {
+    type: "",
+    damage: 5
+  },
+  accuracy: 0.5
+};
+
+const enemy3 = {
+  id: "enemy3",
+  name: "Redbeard",
+  health: 15,
+  weapon: {
+    type: "",
+    damage: 1
+  },
+  accuracy: 0.7
 };
 
 function rest(objHero) {
   objHero.health = 10;
-  console.log(objHero);
   return objHero;
 };
 
@@ -36,6 +69,41 @@ function changeName(objHero) {
   }
   objHero.name = inputField.value;
   inputField.value = null;
+}
+
+function fight(objHero, objEnemy) {
+
+  if (!objHero.name) {
+    alert("Please fill in you hero name first");
+    return false;
+  }
+
+  while (objHero.health > 0 && objEnemy.health > 0) {
+    attack(objHero, objEnemy);
+    attack(objEnemy, objHero);
+    console.log(`Health: ${objHero.name}: ${objHero.health} - ${objEnemy.name}: ${objEnemy.health}`);
+  }
+
+  if (objHero.health < objEnemy.health) {
+    alert("You Lost!")
+    rest(objHero);
+    rest(objEnemy);
+  } else {
+    alert("You won!");
+    const enemyElement = document.getElementById(objEnemy.id);
+    enemyElement.remove();
+    rest(objHero);
+  }
+}
+
+function attack(attacker, defender) {
+  const random = Math.random();
+  if (random <= attacker.accuracy) {
+    console.log(`${attacker.name} hits ${defender.name}`);
+    defender.health -= attacker.weapon.damage
+  } else {
+    console.log(`${attacker.name} misses!`);
+  }
 }
 
 // Write displayStats function that writes your hero's name, health, weapontype, weapon damage to the page. Call it at the end of your script
